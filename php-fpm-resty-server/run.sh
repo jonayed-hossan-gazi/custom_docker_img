5cf02a118e7dc8700f5be717128db3e8b1e61952
Zunaid Hossan Gazi <me@zuna.id>
2024-09-01 18:25:37 +0600

    R2CDN Server Update

--- php-fpm-resty-server/run.sh
+++ php-fpm-resty-server/run.sh
@@ -1,1 +1,3 @@
 docker run -it --rm -p 8080:80 jonayedhossan/php-fpm-resty-server:latest
+#docker run -d --restart=always -p 8080:80 -v /root/wapka_fm:/var/www/app/src:rw jonayedhossan/php-fpm-resty-server:latest
+#docker run -d --restart=always -p 2122:80 -d  -e 'STATIC_SERVER=yes' -v /root/filemanager:/var/www/app/public:ro jonayedhossan/php-fpm-resty-server:latest
